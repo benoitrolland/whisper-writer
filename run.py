84e0cf1c299d7fb0875ebfbd81b693a574823a3d
benoit.rolland <benoit.rolland@free.fr>
2024-03-18 00:29:58 +0100

    exe attempts

--- run.py
+++ run.py
@@ -6,4 +6,16 @@ import subprocess
 os.environ['PYTHONUNBUFFERED'] = '1'
 
 print('Starting WhisperWriter...')
-subprocess.run([sys.executable, os.path.join('src', 'main.py')])
+
+# ko: multiple processes launched
+# subprocess.run([sys.executable, os.path.join('src', 'main.py')])
+
+# ok: but needs python in env:
+#subprocess.run(['python', os.path.join('src', 'main.py'), os.environ['PARAM']])
+
+# ko: [49856] Cannot create temporary directory!
+# if compile with the following --runtime-tmpdir option, loop again:
+# pyinstaller -F run.py --runtime-tmpdir .
+si = subprocess.STARTUPINFO()
+si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+subprocess.run([sys.executable,os.path.join('src','main.py'),os.environ['PYTHONUNBUFFERED']],env={'PYTHONUNBUFFERED':os.environ['PYTHONUNBUFFERED']},stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,startupinfo=si)
